5e7f6556eb7523a60cc2b849b7200814dac176d4
JoseJunio <zeh.junior@hotmail.com>
2018-02-19 11:13:11 -0300

    teste

--- src/main/java/com/ronda/tables/utils/ServiceMap.java
+++ src/main/java/com/ronda/tables/utils/ServiceMap.java
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping(consumes = "application/json", produces = "application/json")
-@CrossOrigin(origins = "https://ronda-back.herokuapp.com", allowCredentials="false")
+//@CrossOrigin(origins = "https://ronda-back.herokuapp.com", allowCredentials="false")
+@CrossOrigin(origins = "https://ronda-front.herokuapp.com", allowCredentials="false")
 public interface ServiceMap {
 	
 }
